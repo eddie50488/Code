843e03104718f184b19279c9792ff7ec152f1250
Edward Young <eddie50488@gmail.com>
2024-03-13 13:04:39 +0000

    Modified URDF

--- omni_drive_arduino/bringup/launch/omnibot.launch.py
+++ omni_drive_arduino/bringup/launch/omnibot.launch.py
@@ -1,38 +1,15 @@
-# Copyright 2020 ros2_control Development Team
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from launch import LaunchDescription
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
-
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
 
-    robot_controller = PathJoinSubstitution(
-        [
-            FindPackageShare("omni_drive_arduino"),
-            "config",
-            "omni_controllers.yaml",
-        ]
-    )
-
     rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("omni_drive_arduino"), "rviz", "diffbot.rviz"]
+        [FindPackageShare("omni_drive_arduino"), "rviz", "omnibot.rviz"]
     )
 
     robot_description_content = Command(
@@ -40,52 +17,68 @@ def generate_launch_description():
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare("omni_drive_arduino"), "urdf", "diffbot.urdf.xacro"]
+                [FindPackageShare("omni_drive_arduino"), "urdf", "omnibot.urdf.xacro"]
             ),
         ]
     )
 
-    log_level = {"log_level": "debug"} 
-
     robot_description = {"robot_description": robot_description_content}
 
-    robot_state_pub_node = Node(
+    robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
+        name="joint_state_publisher",
         output="both",
-        parameters=[robot_description, log_level],
-        
+        parameters=[robot_description],
     )
-    
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        output="both",
+    )
+
+    robot_controller = PathJoinSubstitution(
+        [
+            FindPackageShare("omni_drive_arduino"),
+            "config",
+            "omni_controller_config.yaml",
+        ]
+    )
+
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description, robot_controller, log_level],
+        parameters=[robot_description, robot_controller],
         output="both",
     )
-    
-    # rviz_node = Node(
-    #     package="rviz2",
-    #     executable="rviz2",
-    #     name="rviz2",
-    #     output="log",
-    #     arguments=["-d", rviz_config_file],
-    # )
 
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
 
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["omnidirectional_controller", "--controller-manager", "/controller_manager"],
+        arguments=[
+            "omnidirectional_controller",
+            "--controller-manager",
+            "/controller_manager",
+        ],
         output="both",
     )
 
-
     nodes = [
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        rviz_node,
         control_node,
-        robot_state_pub_node,
         robot_controller_spawner,
-        # rviz_node
     ]
 
     return LaunchDescription(nodes)
