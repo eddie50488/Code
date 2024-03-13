45c4c5108bea2ea93923dae3b0e28dfd87699cac
Edward Young <eddie50488@gmail.com>
2024-03-13 15:33:13 +0000

    Modified TF Tree

--- omni_drive_arduino/bringup/launch/omnibot.launch.py
+++ omni_drive_arduino/bringup/launch/omnibot.launch.py
@@ -8,10 +8,6 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("omni_drive_arduino"), "rviz", "omnibot.rviz"]
-    )
-
     robot_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
@@ -24,13 +20,6 @@ def generate_launch_description():
 
     robot_description = {"robot_description": robot_description_content}
 
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="joint_state_publisher",
-        output="both",
-        parameters=[robot_description],
-    )
 
     joint_state_publisher_node = Node(
         package="joint_state_publisher",
@@ -66,7 +55,6 @@ def generate_launch_description():
     )
 
     nodes = [
-        robot_state_publisher_node,
         joint_state_publisher_node,
         control_node,
         robot_controller_spawner,
